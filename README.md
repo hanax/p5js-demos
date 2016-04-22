742e4000145a1fd3381e673fbb4c357487f71543
hanax <alfalfa.xue@gmail.com>
2016-04-22 16:23:38 -0400

    add music viz demo

--- README.md
+++ README.md
@@ -5,7 +5,9 @@
 * [Drawing with Leap Motion](http://hanax.github.io/p5js-demos/demo_leapmotion)
   * A visualization of hand movement
   * Video demo: https://twitter.com/A_lfalfa/status/708755403727831040
-* [Visualizing music](https://hanax.github.io/p5js-demos/demo_music)
-  * Need to be deployed on https/localhost
+* [Breathe...](https://hanax.github.io/p5js-demos/demo_music)
+  * Use mic as input. Need to be deployed on https or localhost.
   * Video demo: https://twitter.com/A_lfalfa/status/721078799262183424
 ![preview](http://hanax.github.io/p5js-demos/demo_pic/demo.png)
+* [Simple Music Viz](https://hanax.github.io/p5js-demos/demo_music_viz)
+![preview](http://hanax.github.io/p5js-demos/demo_pic/viz.jpg)
---
+++ js/main_music_viz.js
@@ -0,0 +1,70 @@
+var sound = null;
+var amplitude, cnv;
+var idx_x, idx_y;
+var hue;
+
+function preload(){
+  // sound = loadSound('./assets/beautiful_freak.mp3');
+}
+
+function setup() {
+  idx_x = 0;
+  idx_y = 0;
+  cnv = createCanvas(windowWidth, windowHeight);
+  cnv.drop(gotFile);
+
+  amplitude = new p5.Amplitude();
+
+  cnv.mouseClicked(function() {
+    if (sound) {
+      if (sound.isPlaying() ){
+        sound.pause();
+      } else {
+        sound.play();
+      }
+    } else {
+      sound = loadSound('./assets/beautiful_freak.mp3');
+    }
+  });
+}
+
+function draw() {
+  if (sound && sound.isPlaying()) {
+    fill(255);
+    var level = amplitude.getLevel();
+    var l = map(level, 0, 0.5, 0, 255);
+
+    colorMode(HSB, 255);
+    var c = color(hue, l, l);
+    // var c = color(l, l, l);
+
+    stroke(c);
+
+    line(idx_x, idx_y, idx_x, idx_y + 20);
+    if (idx_x == windowWidth) {
+      idx_x = 0;
+      idx_y += 21;
+      hue = (hue + 10) % 255;
+    }
+    idx_x ++;
+  }
+}
+
+function gotFile(file) {
+  if (sound) {
+    sound.dispose();
+  }
+  sound = loadSound(file, function() {
+    background(0);
+    idx_x = 0;
+    idx_y = Math.max(0, windowHeight / 2 - sound.duration() / 35 * 20);
+    hue = 255 * Math.random();
+    sound.play();
+  });
+}
+
+$(document).ready(function() { 
+  $('#btn-save').click(function() {
+    saveCanvas(canvas,'viz','jpg');
+  });
+});
